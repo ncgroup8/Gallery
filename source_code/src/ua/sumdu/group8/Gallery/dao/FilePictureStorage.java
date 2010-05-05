5366df2cf5f59dc4e15d7ace1722734de282e623
Alex <ice-pro@mail.ru>
2010-05-05 23:22:15 +0300

    Added FilePictureStorage class (1st try) Added required libraries
    Signed-off-by: Alex <ice-pro@mail.ru>

---
+++ source_code/src/ua/sumdu/group8/Gallery/dao/FilePictureStorage.java
@@ -0,0 +1,91 @@
+package ua.sumdu.group8.Gallery.dao;
+
+import ua.sumdu.group8.Gallery.dao.exceptions.*;
+import ua.sumdu.group8.Gallery.*;
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+import java.io.*;  
+import java.util.*;
+
+import org.apache.commons.fileupload.FileItem; 
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;  
+import org.apache.commons.fileupload.servlet.ServletFileUpload;  
+
+
+/**
+ * Provides common methods to access picture storage.
+ *
+ * @author Anufriev Alexey
+ * @version 1.0
+ * created 05-May-2010
+ */
+public class FilePictureStorage implements IPictureStorage {
+
+    private Random random = new Random(); 
+
+    /**
+     * Stores a new picture.
+     * 
+     * @param pic a picture to store.
+     * @param req a request containing picture.
+     * @exception PictureStorageException.
+     */
+    public void store(IGalleryPicture pic, HttpServletRequest req) 
+            throws PictureStorageException {
+                
+		if (!ServletFileUpload.isMultipartContent(req)) {
+            throw new PictureStorageException("Server error:" + HttpServletResponse.SC_BAD_REQUEST);
+		}
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setSizeThreshold(1024 * 1024);
+		File tempDir = new File("/temp/");
+		factory.setRepository(tempDir);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		upload.setSizeMax(1024 * 1024 * 10);
+		try {
+			List items = upload.parseRequest(req);
+			Iterator iter = items.iterator();
+			while (iter.hasNext()) {
+			    FileItem item = (FileItem) iter.next();
+			    if (!item.isFormField()) {		    	
+			        pic.setURL(processUploadedFile(item));
+			    }
+			}			
+		} catch (Exception e) {
+			throw new PictureStorageException(e);
+		}		    
+    }
+
+	private String processUploadedFile(FileItem item) throws Exception {
+    
+		File uploadetFile = null;
+        String path = null;
+		do {
+			path = ("/upload/" + random.nextInt() + item.getName());					
+			uploadetFile = new File(path);		
+		} while(uploadetFile.exists());
+		uploadetFile.createNewFile();
+		item.write(uploadetFile);
+        return path;
+	}
+
+    /**
+     * Removes a specified picture.
+     * 
+     * @param pic a picture to remove.
+     * @exception PictureStorageException.
+     */
+    public void remove(IGalleryPicture pic) throws PictureStorageException {
+        
+        File f = new File(pic.getURL());
+        if (!f.exists()) {
+            throw new PictureStorageException("No such file or directory: " + pic.getURL());
+        }
+        if (!f.canWrite()) {
+            throw new PictureStorageException("Write protected: " + pic.getURL());
+        }
+        if (!f.delete()) {
+            throw new PictureStorageException("Deletion failed.");
+        }
+    }
+}
